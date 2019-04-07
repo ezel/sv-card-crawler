a8952a303b30d1a5bdb26f7469df242026c4f700
Martin Shi <ezel.db@gmail.com>
2019-04-07 22:45:48 +0800

    add argparse add usage for main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from listCrawler import getCardList
 from cardCrawler import fetchSingleCard
 from model import Card, init_tables
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("mode", type=int, choices=[1,3], default=3,
+                    help="game mode of the card: 1=Rotation, 3=Unlimit")
+parser.add_argument("language", type=str, choices=['en','ja','zh-tw'], default='en',
+                    help="card language: en=english, ja=janpanse, zh-tw=traditional chinese")
+parser.add_argument("-f","--forceFetch", action="store_true",
+                    help="wheather fetch from web or use local temp file")
+args = parser.parse_args()
 
 if __name__ == "__main__":
     init_tables()
-    lang = 'en'
-    currentList = getCardList(3, forceFetch=True)
+    lang = args.language
+    currentList = getCardList(args.mode, forceFetch=args.forceFetch)
     for cid in currentList:
         if Card.checkNotExist(cid, lang):
             currentCard = fetchSingleCard(cid)
