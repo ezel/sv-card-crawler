55492b6ca0957ea5d418e941929e481ebaaf0242
Martin Shi <ezel.db@gmail.com>
2019-04-12 00:17:17 +0800

    improve card.html

--- viewer.py
+++ viewer.py
@@ -11,15 +11,19 @@ def index():
 def test():
     return 'Test here'
 
-@app.route('/card')
 @app.route('/card/<cid>')
-def card(cid=0):
-    cards = [c for c in Card.select().where(Card.cid == cid) ]
-    if len(cards) > 0:
-        return render_template('card.html', cards=cards)
+def card(cid):
+    lang = request.args.get('lang') or 'en'
+    card = Card.select().where(Card.cid == cid).where(Card.language == lang).first()
+    if card:
+        return render_template('card.html', card=card)
     else:
-        return 'no card find with %s' % cid
+        return 'no %s card find with %s' % (lang, cid)
 
 @app.route('/sql')
 def sql():
+    pass
+
+@app.route('cards')
+def cards():
     pass
